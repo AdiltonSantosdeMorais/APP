020a544f07f99dd9edf1317ca9e1dceaf18dbdef
AdiltonSantosdeMorais <94267843+AdiltonSantosdeMorais@users.noreply.github.com>
2026-05-02 21:40:15 -0400

    INSTALAR AGORA

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'elecnor-hse-v3';
+const CACHE_NAME = 'elecnor-vFinal';
 const ASSETS = [
   '/',
   '/index.html',
@@ -7,13 +7,9 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
