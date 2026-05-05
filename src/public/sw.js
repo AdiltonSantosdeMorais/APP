2cf153eb617735dbdd197589433eb4efc20e3b04
AdiltonSantosdeMorais <94267843+AdiltonSantosdeMorais@users.noreply.github.com>
2026-05-04 21:46:18 -0400

    atu

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,6 +1,4 @@
-// Sempre que fizer uma mudança importante, mude esse número (ex: v1.1, v1.2)
-const CACHE_NAME = 'elecnor-hse-v1.2'; 
-
+const CACHE_NAME = 'elecnor-v3.0';
 const ASSETS = [
   '/',
   '/index.html',
@@ -8,31 +6,19 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
-// Instalação e Cache
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Força o SW novo a assumir o controle imediatamente
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Limpeza de Caches Antigos (Resolve o seu problema de abrir o formulário velho)
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key); // Apaga as versões antigas
-          }
-        })
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.map((key) => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    }));
+  }));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
