d4871f9f38be15a0a6372e8759cc4ed9b22a0d2f
AdiltonSantosdeMorais <94267843+AdiltonSantosdeMorais@users.noreply.github.com>
2026-05-06 05:19:50 -0400

    OK

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,29 +1,15 @@
-const CACHE_NAME = 'hse-v5';
+const CACHE_NAME = 'hse-elecnor-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
   './logo-elecnor.png',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => (key !== CACHE_NAME ? caches.delete(key) : null))
-    ))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
